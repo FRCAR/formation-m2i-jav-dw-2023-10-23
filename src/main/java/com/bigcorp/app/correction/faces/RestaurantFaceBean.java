7a44f0ca9eca6a81dd3036b535cb271dada80df5
François Caremoli <francois.caremoli@wanadoo.fr>
2023-10-24 15:06:52 +0200

    Correction formulaire restaurants

--- src/main/java/com/bigcorp/app/correction/faces/RestaurantFaceBean.java
+++ src/main/java/com/bigcorp/app/correction/faces/RestaurantFaceBean.java
@@ -10,9 +10,12 @@ import jakarta.inject.Named;
 public class RestaurantFaceBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+
 	private String message = "Bienvenue au restaurant";
-	private String loadId;
-	
+	private Long loadId;
+
+	private RestaurantFormBean restaurantFormBean = new RestaurantFormBean();
+
 	public String getMessage() {
 		return message;
 	}
@@ -25,17 +28,32 @@ public class RestaurantFaceBean implements Serializable {
 		System.out.println("Création d'un restaurant");
 		return "new-restaurant";
 	}
-	
+
 	public void onLoad() {
 		System.out.println("Je charge le restaurant avec l'id : " + this.loadId);
+		this.restaurantFormBean.setId(this.loadId);
+		this.restaurantFormBean.setName("le restaurant avec l'id : " + this.loadId);
 	}
 
-	public String getLoadId() {
+	public Long getLoadId() {
 		return loadId;
 	}
 
-	public void setLoadId(String loadId) {
+	public void setLoadId(Long loadId) {
 		this.loadId = loadId;
+	}
+
+	public RestaurantFormBean getRestaurantFormBean() {
+		return restaurantFormBean;
+	}
+
+	public void setRestaurantFormBean(RestaurantFormBean restaurantFormBean) {
+		this.restaurantFormBean = restaurantFormBean;
+	}
+
+	public String save() {
+		System.out.println("Je sauvegarde le restaurantFormBean : " + this.restaurantFormBean);
+		return "restaurant?faces-redirect=true&id=" + this.restaurantFormBean.getId();
 	}
 
 }
