6c956ad6e2df13ca387e20d20cfd391ec936e765
François Caremoli <francois.caremoli@wanadoo.fr>
2023-10-24 11:10:38 +0200

    Ajout MonPremierFormBean

---
+++ src/main/java/com/bigcorp/app/correction/servlet/MonPremierFormBean.java
@@ -0,0 +1,28 @@
+package com.bigcorp.app.correction.servlet;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
+@Named
+@RequestScoped
+public class MonPremierFormBean implements Serializable {
+
+	private String maValeur;
+
+	public MonPremierFormBean() {
+		System.out.println("Je suis MonPremierFaceBean, et j'ai été instancié");
+		this.maValeur = "Salut, on est le  : " + LocalDateTime.now(); 
+	}
+
+	public String getMaValeur() {
+		return maValeur;
+	}
+
+	public void setMaValeur(String maValeur) {
+		this.maValeur = maValeur;
+	}
+
+}
